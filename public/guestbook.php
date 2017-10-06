122642a6b44f419212090561fe4a2620578c432f
Remdran <kelsyerg@gmail.com>
2017-10-06 14:36:37 +0100

    Adding some quick styling

--- public/guestbook.php
+++ public/guestbook.php
@@ -4,16 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>Guest Book</title>
 </head>
 <body>   
-    <?php 
-        $sql = $link->query("*", "messages");
+    <div class="container">
+       
+                
+        <?php
+            $sql = $link->query("*", "messages");
+            
+            foreach ($sql as $row) { 
+        ?>
+                <article>
+                    <h4>
+                        <?= $row['user_name'] . " " . $row['created_at'] . " Posted: " ?>
+                    </h4>
+                    <div class="body"><?=  $row['comment'] . "<hr>" ?></div>
+                </article>
+            <?php
+                }
+            ?>    
 
-        foreach($sql as $row) {
-            echo $row['user_name'] . $row['created_at'] . "Posted: " . $row['comment'] . "<br>";
-        }
-    ?>    
-
-    </body>
+    </div>
+</body>
 </html>
