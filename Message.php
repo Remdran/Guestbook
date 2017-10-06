02b96225f46469ef010adb8d9c9539775f57e6dc
Remdran <kelsyerg@gmail.com>
2017-10-06 11:53:29 +0100

    Initial setup of files

---
+++ Message.php
@@ -0,0 +1,24 @@
+<?php   
+
+class Message 
+{
+    public function index()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function show()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
+}
